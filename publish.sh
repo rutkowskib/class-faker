b9924458229f654f1aabe015b248e14cbd95a174
Bartlomiej Rutkowski <brutkowski@tilt.app>
2022-08-21 17:41:50 +0200

    Add readme to publish

--- publish.sh
+++ publish.sh
@@ -1,5 +1,6 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 cp $SCRIPT_DIR/package.json $SCRIPT_DIR/dist/package.json
+cp $SCRIPT_DIR/Readme.md $SCRIPT_DIR/dist/Readme.md
 cd $SCRIPT_DIR/dist
 npm publish --access public
